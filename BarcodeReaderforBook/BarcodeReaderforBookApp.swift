bdfba32868f0a863cc94a2f85b69c164b08ed42a
AmmyK <kojikeisisu@gmail.com>
2023-02-27 21:36:23 +0900

    Initial Commit

---
+++ BarcodeReaderforBook/BarcodeReaderforBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeReaderforBookApp.swift
+//  BarcodeReaderforBook
+//
+//  Created by amamiya on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeReaderforBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
